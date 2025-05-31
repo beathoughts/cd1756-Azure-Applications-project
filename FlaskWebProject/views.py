3c4c29367e0460ae8d29430ce8ddcec380a0a630
beathoughts <124046397+beathoughts@users.noreply.github.com>
2025-05-31 13:21:38 +0800

    Update views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -121,7 +121,6 @@ def _load_cache():
     return cache
 
 def _save_cache(cache):
-    def _save_cache(cache):
     # TODO: Save the cache, if it has changed
     if cache.has_state_changed:
         session['token_cache'] = cache.serialize()
